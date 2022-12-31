f5d8220fd698681a83f019b8d13b65164d3f0a9a
Mario Stoev <marios1@marios15MD6R.vmware.com>
2022-12-31 13:33:33 +0200

    logistics project

--- LogisticCompany/src/main/java/uni/nbu/logistic/company/model/UserRole.java
+++
@@ -1,5 +0,0 @@
-package uni.nbu.logistic.company.model;
-
-public enum UserRole {
-    OFFICE_EMPLOYEE, COURIER, CLIENT, UNKNOWN
-}
--- LogisticCompany/src/main/resources/DB/employee-db.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS employee;
-CREATE TABLE employee (
-                      origin_id CHAR(36) NOT NULL PRIMARY KEY,
-                      username VARCHAR(255) NULL,
-                      company_name VARCHAR(255) NULL,
-                      role VARCHAR(255) NOT NULL,
-);
